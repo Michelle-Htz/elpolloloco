bda372ef70edfa5c2a7b40c97e676f53056282ec
Michelle Heintze <michelle-heintze@web.de>
2021-08-27 08:20:58 +0200

    added new data

--- js/game.js
+++ js/game.js
@@ -8,5 +8,8 @@ function init() {
     ctx = canvas.getContext('2d');
     character.src = '../img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png';
 
+
     ctx.drawImage(character, 20, 20, 50, 150);
+
+
 }
