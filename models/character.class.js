f0f01e62fcbac4fe2bba65a6ab77ef35bc23b055
Michelle Heintze <michelle-heintze@web.de>
2021-08-27 19:00:34 +0200

    added images and size

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,7 @@
 class Character extends MovableObject {
-
+    height = 350;
+    width = 140;
+    y = 60;
 
     constructor() {
         super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png');
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,9 @@
 class Chicken extends MovableObject {
 
+    height = 90;
+    width = 80;
+    y = 320;
+
     constructor() {
         //super() benutzt man, wenn man Methoden des übergeordnete Objekt aufruft
         super().loadImage('img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png');
