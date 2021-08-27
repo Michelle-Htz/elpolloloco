fd2b0798ef05950add0895d97b121dd16a701235
Michelle Heintze <michelle-heintze@web.de>
2021-08-27 14:05:09 +0200

    add images

--- models/world.class.js
+++ models/world.class.js
@@ -5,9 +5,31 @@ class World {
         new Chicken(),
         new Chicken(),
     ];
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas; //das obere Canvas wird in dieses Canvas reingeschrieben, so kann ich mit this auf height und width zugreifen. 
+        this.draw();
+
+    }
 
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); //Canvas wird immer wieder geleert
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        this.enemies.forEach(enemy => { //es wird auf jedes Chicken einzeln zugegriffen
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        })
+
+
+        //draw wird immer wieder aufgerufen
+        let self = this; //in dieser Funktion kennt er this nicht mehr daher wird eine neue Variable erstellt und diese kann ich weiter geben.
+        requestAnimationFrame(function() { //Diese Funktion wird etwas später ausgeführt, nachdem bereits alles geladen wurde.
+            self.draw();
+
+        });
 
     }
 }
